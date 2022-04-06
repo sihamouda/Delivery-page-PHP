8756df349a9c7b9b930db4d30c6ca5f5fa64e91f
sihamouda <anis.c.hamouda@gmail.com>
2022-04-06 22:33:01 +0100

    end part 2

--- feedback/index.php
+++ feedback/index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FeedBack</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
 </head>
 
 <?php 
@@ -18,13 +21,14 @@ if ( isset ($_POST['submit'])){
 }}
 ?>
 
-<body>
+<body class="container">
     <form action="?" method="post">
         <h1>Feedback</h1>
         <input type="radio" name="vote[ ]" value="g">Good
         <input type="radio" name="vote[ ]" value="n">Neutral
         <input type="radio" name="vote[ ]" value="b">Bad
-        <button name="submit">vote!</button>
+        <br/>
+        <button name="submit" type="button" class="btn btn-primary">vote!</button>
     </form>
 </body>
 </html>
