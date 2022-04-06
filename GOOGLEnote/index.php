6ebae590a3028a5dd38782b2a7b475e553bba4bd
sihamouda <anis.c.hamouda@gmail.com>
2022-04-06 22:49:34 +0100

    html done

---
+++ GOOGLEnote/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
+    <title>Add Note</title>
+</head>
+<body >
+    <div class="container">
+        <div class="row">
+            <div class="col-6"> 
+                <form  action="?" method="POST">
+                    <h1>Note</h1>
+                    <textarea rows="4" cols="50" name = "note"></textarea><br>
+                    <button name="submit" value="submit" type="button" class="btn btn-primary">
+                        Add Note
+                    </button>
+                </form>
+            </div>
+            <div class="col-6"></div>
+            
+        </div>
+    </div>
+</body>
+</html>
