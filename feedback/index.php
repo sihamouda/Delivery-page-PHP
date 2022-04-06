6a2e062c7d6d529097dc00b44817a5f931ef33bf
sihamouda <anis.c.hamouda@gmail.com>
2022-04-06 22:28:21 +0100

    php done

--- feedback/index.php
+++ feedback/index.php
@@ -5,13 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FeedBack</title>
 </head>
+
+<?php 
+if ( isset ($_POST['submit'])){
+    if(!empty($_POST['vote'])){
+      if(isset($_COOKIE['vote'])){
+        echo "<script>
+            alert('You have already voted')    
+        </script>";
+      }else{
+       setcookie("vote",$_POST['vote'][0],time()+120);}
+}}
+?>
+
 <body>
     <form action="?" method="post">
         <h1>Feedback</h1>
-        <input type="radio" name="vote">Good
-        <input type="radio" name="vote">Neutral
-        <input type="radio" name="vote">Bad
-        <button>vote!</button>
+        <input type="radio" name="vote[ ]" value="g">Good
+        <input type="radio" name="vote[ ]" value="n">Neutral
+        <input type="radio" name="vote[ ]" value="b">Bad
+        <button name="submit">vote!</button>
     </form>
 </body>
 </html>
