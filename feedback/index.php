5cf996b8386fe71435edf46aca5d1eda1cef287b
sihamouda <anis.c.hamouda@gmail.com>
2022-04-06 22:11:43 +0100

    start part 2 html done

---
+++ feedback/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FeedBack</title>
+</head>
+<body>
+    <form action="?" method="post">
+        <h1>Feedback</h1>
+        <input type="radio" name="vote">Good
+        <input type="radio" name="vote">Neutral
+        <input type="radio" name="vote">Bad
+        <button>vote!</button>
+    </form>
+</body>
+</html>
